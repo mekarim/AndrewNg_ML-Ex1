2e40962ce6e69c2a9f1293ab0af68cb214592267
M Ehsan Karim <ehsan.mce@gmail.com>
2016-02-08 23:12:32 +0100

    Implemented and submitted the main requirement: (1) cost computation and (2) gradient descent.

--- gradientDescent.m
+++ gradientDescent.m
@@ -16,13 +16,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+    
+    temp = ((theta' * X')'-y);
+    theta = theta - (alpha .* sum([temp temp] .* X) ./m)';
+    
     % ============================================================
 
     % Save the cost J in every iteration    
